1c4ed7faa813807c80ae446bce0ef67dd2b139dc
real-Xavier <74474980+real-Xavier@users.noreply.github.com>
2022-11-05 18:19:49 +0800

    Add files via upload

---
+++ prime number.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+int main()
+{
+    int factor, n, x, count = 0;
+    for (n = 2; n <= 250000; n++)
+    {
+        for (x = 2, factor = 0; x <= n / 2; x++)
+        {
+            if (n % x == 0)
+            {
+                factor++;
+                break;
+            }
+        }
+        if (factor == 0)
+        {
+            printf("%d\t", n);
+            count++;
+            if (count%10==0)
+            {
+                printf("\n");
+            }
+        }
+    }
+    printf("\n");
+    time_t now;
+    time(&now);
+    printf("Program build time: %s\n", __TIMESTAMP__);
+    printf("Current local time: %s", ctime(&now));
+    system("pause");
+    return 0;
+}
