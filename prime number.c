690bc980fef69dbfc3baf9e8e92c35743db23a00
real-Xavier <74474980+real-Xavier@users.noreply.github.com>
2022-11-05 18:25:22 +0800

    Update prime number.c

--- prime number.c
+++ prime number.c
@@ -18,7 +18,7 @@ int main()
         {
             printf("%d\t", n);
             count++;
-            if (count%10==0)
+            if (count % 10 == 0)
             {
                 printf("\n");
             }
