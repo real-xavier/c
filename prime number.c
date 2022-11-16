8d5d2eda6c524509fa037fc7794f90fc74a03a2c
real-xavier <74474980+real-xavier@users.noreply.github.com>
2022-11-16 16:17:02 +0800

    Update prime number.c

--- prime number.c
+++ prime number.c
@@ -1,34 +1,24 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#define N 100000
 int main()
 {
-    int factor, n, x, count = 0;
-    for (n = 2; n <= 25000; n++)
+    int n, x, count = 0;
+    for (n = 2; n <= N; n++)
     {
-        for (x = 2, factor = 0; x <= n / 2; x++)
+        for (x = 2; x <= n / 2; x++)
         {
             if (n % x == 0)
-            {
-                factor++;
                 break;
-            }
         }
-        if (factor == 0)
-        {
-            printf("%d\t", n);
-            count++;
-            if (count % 10 == 0)
-            {
-                printf("\n");
-            }
-        }
+        if (n % x == 0)
+            continue;
+        printf("%d\t", n);
+        count++;
+        if (count % 10 == 0)
+            printf("\n");
     }
-    printf("\n");
-    time_t now;
-    time(&now);
-    printf("Program build time: %s\n", __TIMESTAMP__);
-    printf("Current local time: %s", ctime(&now));
+    printf("%d\n", count);
     system("pause");
     return 0;
 }
